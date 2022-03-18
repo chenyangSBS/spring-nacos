96c32b6a355fa7fbbb8e1e3f16c5b582505dee29
shiyunxi <shiyunxi@gome.inc>
2022-03-18 17:10:26 +0800

    upt

---
+++ springboot-nacos-config/src/main/java/com/yx/nacos/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package com.yx.nacos.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RefreshScope
+@Data
+public class DataSourceConfig {
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+}
