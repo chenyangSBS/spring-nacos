5534b5f185f2998480f32ba6e9d4c1be16553fa1
chenyangSBS <chenyangSBS@github.com>
2024-11-28 10:46:35 +0800

    update dependency

--- springcloud-nacos-config/src/main/java/edu/sbs/ms/config/DataSourceConfig.java
+++ springcloud-nacos-config/src/main/java/edu/sbs/ms/config/DataSourceConfig.java
@@ -3,11 +3,13 @@ package edu.sbs.ms.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @RefreshScope
 @Data
+@Configuration
 public class DataSourceConfig {
     @Value("${spring.datasource.url}")
     private String url;
